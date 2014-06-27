f0d5d1029013a8f0ed3cbc3a5226c38c509da9a3
Matt Stein <matt@workingconcept.com>
2014-06-26 21:38:34 -0700

    Added to the mostly non-functional front-end form.

--- public/index.php
+++ public/index.php
@@ -25,17 +25,17 @@ if ( ! empty($_POST))
 	h2{font-size:1.4em;margin:0 0 1em 0;line-height:1.3em;}
 	a:link,a:active,a:visited{border:0;text-decoration:none;color:#DB5B48;}
 	a:hover{text-decoration:underline;color:#DB5B48;}
-	html,body {height:100%;overflow:hidden;}
+	html,body {height:100%;}
 	p {margin: 1em 0;}
 	h1 {font-weight:normal;font-size:2em;line-height:1em;}
 	h3 {font-weight:bold;color:#444;font-size:1em;line-height:1em; margin: 1em 0;}
-	#container{max-width:400px;margin:0 auto;padding:10px 25px;background:#fff;border-radius:5px;border:1px solid #ddd;position:relative;top:10%;}
+	#container{max-width:400px;margin:0 auto 75px auto;padding:10px 25px;background:#fff;border-radius:5px;border:1px solid #ddd;position:relative;top:10%;}
 	button {clear: both;color:#fff;background:#da5a47;border-radius:3px;padding:6px 12px; border:0;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;cursor:pointer;font-size: 0.9em;}
-	button:hover {background: #BF503F;}
-	input[type=text] {border: 1px solid #ccc;padding: 0.4em;width: 100%;clear: both;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;font-size: 0.8em;margin: 0.2em 0;}
-	label {font-size: 0.9em;color: #666;}
-	p.tip {color: #999;font-size: 0.7em;margin: 0.2em 0 0.2em 1.4em;}
-	.control-group {margin: 0.6em 0;clear: both;overflow: hidden;}
+	button:hover {background:#BF503F;}
+	input[type=text] {border:1px solid #ccc;padding:0.4em;width:97%;clear:both;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;font-size:0.8em;margin:0.2em 0;}
+	label {font-size:0.9em;color:#666;}
+	p.tip {color:#999;font-size:0.7em;margin:0.2em 0 0.2em 1.4em;}
+	.control-group {margin:0.6em 0;clear:both;overflow:hidden;}
 	</style>
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -66,19 +66,31 @@ if ( ! empty($_POST))
 				<h3>Include</h3>
 
 				<div class="control-group">				
-					<label><input type="checkbox" name="hasVariables"> Variables</label>
-					<p class="tip">Even the simplest of plugins will use variables.</p>
+					<label><input type="checkbox" name="hasVariables" checked> Template Variables [<a href="http://buildwithcraft.com/docs/plugins/variables" target="_blank">?</a>]</label>
+					<p class="tip">Even the simplest of plugins will probably use template variables.</p>
 				</div>
 				<div class="control-group">
-					<label><input type="checkbox" name="hasServices"> Services</label>
+					<label><input type="checkbox" name="hasServices"> Services [<a href="http://buildwithcraft.com/docs/plugins/services" target="_blank">?</a>]</label>
 					<p class="tip">Most plugins will use services.</p>
 				</div>
 				<div class="control-group">
-					<label><input type="checkbox" name="hasControllers"> Controllers</label>
+					<label><input type="checkbox" name="hasServices"> Models [<a href="http://buildwithcraft.com/docs/plugins/models" target="_blank">?</a>]</label>
+					<p class="tip">If you'll be storing data, you'll want containers for it.</p>
+				</div>
+				<div class="control-group">
+					<label><input type="checkbox" name="hasControllers"> Controllers [<a href="http://buildwithcraft.com/docs/plugins/controllers" target="_blank">?</a>]</label>
 					<p class="tip">Plugins with webhooks or other types of system tie-ins will probably utilize controllers.</p>
 				</div>
 				<div class="control-group">
-					<label><input type="checkbox" name="hasFieldTypes"> Field Types</label>
+					<label><input type="checkbox" name="hasControllers"> Records [<a href="http://buildwithcraft.com/docs/plugins/records" target="_blank">?</a>]</label>
+					<p class="tip">Records are like models with a database layer.</p>
+				</div>
+				<div class="control-group">
+					<label><input type="checkbox" name="hasControllers"> Dashboard Widgets [<a href="http://buildwithcraft.com/docs/plugins/widgets" target="_blank">?</a>]</label>
+					<p class="tip">Will you be making a widget available for use on the dashboard?</p>
+				</div>
+				<div class="control-group">
+					<label><input type="checkbox" name="hasFieldTypes"> Field Types [<a href="http://buildwithcraft.com/docs/plugins/field-types" target="_blank">?</a>]</label>
 					<p class="tip">Want to add your own field type(s) that can be used for entry fields?</p>
 				</div>
 				<div class="control-group">
@@ -86,7 +98,7 @@ if ( ! empty($_POST))
 					<p class="tip">Will your plugin have its own section and UI in the control panel?</p>
 				</div>
 				<div class="control-group">
-					<label><input type="checkbox" name="includeComments"> Comments</label>
+					<label><input type="checkbox" name="includeComments" checked> Code Comments</label>
 					<p class="tip">We've added lots of comments within each plugin file if you'd like an orientation or a refresher.</p>
 				</div>
 			</div>
